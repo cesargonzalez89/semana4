80316308aaf94565686ac330f7776de96c24773f
cesargonzalez89 <99153503+cesargonzalez89@users.noreply.github.com>
2022-03-24 17:06:38 -0600

    Add files via upload

--- pokedexLaunchX.js
+++ pokedexLaunchX.js
@@ -1,11 +1,9 @@
 const searchBtn = document.getElementById('search-btn'); // search button
 const inputField = document.getElementById('pokeName'); // search field input
 const nameScreen = document.getElementById('name-screen'); //name-screen
-//const imageScreen = document.getElementById('main-screen'); // image screen
-//const imageScreen = document.getElementById('pokeImg'); // image screen
 const aboutScreen = document.getElementById('about-screen'); // about-text screen
-const typeScreen = document.getElementById('type-screen'); // type screen
-const idScreen = document.getElementById('id-screen'); // spices screen
+const heightScreen = document.getElementById('height-screen'); // height screen
+const weightScreen = document.getElementById('weight-screen'); // spices screen
 
 const fetchPokemon = () =>{
     const pokeName = document.getElementById("pokeName");
@@ -16,9 +14,11 @@ const fetchPokemon = () =>{
         if(res.status!="200"){
             console.log(res);
             nameScreen.innerHTML = "No encontrado";
-            typeScreen.innerHTML = "N/A";
-            idScreen.innerHTML = "";
             aboutScreen.innerHTML = "Sin informacion";
+            heightScreen.innerHTML = ``;
+            weightScreen.innerHTML = ``;
+            document.getElementById("type1").style.backgroundImage = `url('')`;
+            document.getElementById("type2").style.backgroundImage = `url('')`;
             inputField.value = '';
             pokeImage("./sad-pikachu.gif");
             return;
@@ -29,18 +29,24 @@ const fetchPokemon = () =>{
         console.log(data);
         
         let id = ('00' + data.id).slice(-3);
-        //imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
-        
-        //let pokeImg = data.sprites.front_default;
         let pokeImg = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
         //console.log(pokeImg);
         pokeImage(pokeImg);
+        setDescription(id);
+
+         // Set Type
+         if(data.types.length > 0){
+            document.getElementById("type1").style.backgroundImage = `url('${getAssetTypeEs(data.types[0].type.name)}')`;
+        }
+        if(data.types.length > 1){
+            document.getElementById("type2").style.backgroundImage = `url('${getAssetTypeEs(data.types[1].type.name)}')`;
+        } else {
+          document.getElementById("type2").style.backgroundImage = `url('')`;
+        }
+
         nameScreen.innerHTML = data.name;
-        typeScreen.innerHTML = data.types[0].type.name;
-        idScreen.innerHTML = `#${data.id}`;
-        aboutScreen.innerHTML = `Altura: ${data.height * 10}cm Peso: ${
-        data.weight / 10
-        }kg`;
+        heightScreen.innerHTML = `Altura: ${data.height}M`;
+        weightScreen.innerHTML = `Peso: ${data.weight}Kg`;
         inputField.value = '';
     })
 }
@@ -51,7 +57,6 @@ const pokeImage = (url) => {
     console.log(url);
     const pokeImg = document.getElementById("pokeImg");
     pokeImg.src = url;
-    //imageScreen.style.backgroundImage = url;
 }
 
 //pokeImage("");
@@ -62,30 +67,6 @@ const imprimir = () =>{
     console.log("Hola " + pokeInput);
 }
 
-
-
-
-
-
-
-
-
-/*const getPokemonData = (pokemon) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response) => response.json())
-    .then((data) => {
-      let id = ('00' + data.id).slice(-3);
-      imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
-      nameScreen.innerHTML = data.name;
-      typeScreen.innerHTML = data.types[0].type.name;
-      idScreen.innerHTML = `#${data.id}`;
-      aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${
-        data.weight / 10
-      }kg`;
-      inputField.value = '';
-    });
-};*/
-
 inputField.addEventListener(
     'keydown',
     (event) => event.key === 'Enter' && searchBtn.click()
@@ -93,3 +74,70 @@ inputField.addEventListener(
 
 //searchBtn.addEventListener('click', () => getPokemonData(inputField.value));
 searchBtn.addEventListener('click', () => fetchPokemon());
+
+
+
+
+
+
+
+
+
+
+function setDescription(numPokemon){
+    fetch("https://pokeapi.co/api/v2/pokemon-species/" + numPokemon)
+      .then((response) => response.json())
+      .then((data) => {
+        // Set description of Pokemon
+        if(data.flavor_text_entries.find((text_entry) => text_entry.language.name === 'es')){
+          var description = data.flavor_text_entries.find((text_entry) => text_entry.language.name === 'es').flavor_text;
+          description = description.replace(/\n/g, ' ').replace(/\f/g, ' ');
+          aboutScreen.innerHTML= description;
+        }
+      });
+  }
+  
+  function getAssetTypeEs(type){
+      console.log(type);
+    switch(type){
+      case 'normal':
+        return './Tipo_normal.gif';
+      case 'fighting':
+        return './Tipo_lucha.gif';
+      case 'flying':
+        return './Tipo_volador.gif';
+      case 'poison':
+        return './Tipo_veneno.gif';
+      case 'ground':
+        return './Tipo_tierra.gif';
+      case 'rock':
+        return './Tipo_roca.gif';
+      case 'bug':
+        return './Tipo_bicho.gif';
+      case 'ghost':
+        return './Tipo_fantasma.gif';
+      case 'steel':
+        return './Tipo_acero.gif';
+      case 'fire':
+        return './Tipo_fuego.gif';
+      case 'water':
+        return './Tipo_agua.gif';
+      case 'grass':
+        return './Tipo_planta.gif';
+      case 'electric':
+        return './Tipo_el%C3%A9ctrico.gif';
+      case 'psychic':
+        return './Tipo_ps%C3%ADquico.gif';
+      case 'ice':
+        return './Tipo_hielo.gif';
+      case 'dragon':
+        return './Tipo_drag%C3%B3n.gif';
+      case 'dark':
+        return './Tipo_siniestro.gif';
+      case 'fairy':
+        return './Tipo_hada.gif';
+      default:
+        return './UnknownIC_Big.png';
+    }
+  }
+  
